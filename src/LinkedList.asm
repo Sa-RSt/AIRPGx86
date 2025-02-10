cee4be817475c03285fbe8efd00d7f0fab792df2
Fraise <fmmuller2004@gmail.com>
2025-02-09 23:37:35 -0300

    Update LinkedList.asm
    Placeholder changes, expect more soon. I'm working on a separate file

--- src/LinkedList.asm
+++ src/LinkedList.asm
@@ -1,1 +1,11 @@
+section .data
 
+section .bss
+    random_byte resb 1
+
+section .text
+    %ifdef TESTING
+        global _start
+        %define DEBUG 1
+    %endif
+    %include "stdlib_macros.asm" 
