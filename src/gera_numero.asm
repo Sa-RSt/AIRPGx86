37c04f59c02beeed60cba5c40a0642b7451f060c
Sa-RSt <sasha.rodelasteidle@gmail.com>
2025-02-23 20:45:20 -0300

    Fix gera_numero

--- src/gera_numero.asm
+++ src/gera_numero.asm
@@ -1,5 +1,8 @@
 section .data
 
+    gera_numero_dev_urandom: db "/dev/urandom", 0
+    
+
 section .bss
     random_byte resb 1
 
@@ -11,16 +14,17 @@ section .text
     %include "stdlib_macros.asm"           ; Inclui o arquivo de macros
 
 %ifdef TESTING
-;_start:
+_start:
     mov rdi, 20         ; Valor de n para a função gera_numero
     call gera_numero
+    print_registers rdx
     call exit
 %endif
 
 ; Devolve um número entre 1 e n. O valor máximo de n é 256.
 ; Recebe o valor de n em rdi, retorna o numero sorteado em rdx.
 gera_numero:
-    multipush rax, rbx, rcx, rsi
+    multipush rax, rbx, rcx, rsi, r14, r15
 
     ; Casos de erro
     cmp rdi, 256
@@ -28,46 +32,45 @@ gera_numero:
     cmp rdi, 1
     jl .erro_minimo
 
-    ; Balanceia o intervalor de 0 a 255 para ter a mesma chance de se obter um número de 1 a n
-    mov rdx, 0                  ; Parte superior do dividendo
-    mov rax, 256                ; Parte inferior do dividendo
-    div rdi                     ; Divisor (equivalente ao valor de n)
-                                
-    ; Determina o valor máximo do número aleatório aceito
-    mov rsi, 256
-    sub rsi, rdx                ; Resto da divisão fica em rdx
-    sub rsi, 1                  ; Subtrai 1 pois foi calculado de 1 a 256 e não de 0 a 255
+    mov r15, rdi
 
-    ; Sorteia o número - Por algum motivo só funciona na arquitetura 32 bits em 64 bits o getrandom falha
-    .loop:
-        mov eax, 355                ; Código syscall getrandom
-        lea ebx, [random_byte]      ; Endereço onde será 
-        mov ecx, 1                  ; Ler 1 byte
-        mov edx, 0                  ; Sem flags
-        int 0x80                    ; Chamada do sistema 32 bits
+    mov rax, 2  ; sys_open
+    mov rdi, gera_numero_dev_urandom
+    xor rsi, rsi  ; O_RDONLY
+    xor rdx, rdx
+    syscall
+    
+    mov r14, rax  ; guarda fd no r14
+    
+    xor rax, rax  ; sys_read
+    mov rdi, r14
+    mov rsi, random_byte
+    mov rdx, 1
+    syscall
+    
+    mov rax, 3  ; sys_close
+    mov rdi, r14
+    syscall
 
-        movzx eax, byte [random_byte]   ; Move o valor gerado para eax expandindo os zeros
-        cmp eax, esi                    ; Compara com o valor máximo
-        jg .loop                        ; Se for maior, pega outro número
+    xor rax, rax
+    mov al, [random_byte]   ; Move o valor gerado para eax expandindo os zeros
+    print_registers rax
 
     ; Calcula o resto do número sorteado por n
-    mov rdx, 0                  ; Parte superior do dividendo
-    mov eax, eax                ; Parte inferior do dividendo
-    div rdi                     ; Divisor
+    mov rdx, 0                  ; Parte superior do dividendo (parte inferior está no rax)
+    div r15                     ; Divisor
 
     add rdx, 1                  ; Soma 1 para ficar de 1 a n
 
     ; printf "uc", rdx, 0x0A
 
-    multipop rax, rbx, rcx, rsi
+    multipop rax, rbx, rcx, rsi, r14, r15
     ret
 
     .erro_maximo:
         print_literal "O valor máximo permitido para n é 256", 0x0A
-        multipop rax, rbx, rcx, rsi
         call exit
 
     .erro_minimo:
         print_literal "Não é possível sortear para valores menores ou iguais a zero", 0x0A
-        multipop rax, rbx, rcx, rsi
         call exit
